2ef40852869a7accd273d6aa8f3484ce36343b8a
Christian Paul <christianspaul72@gmail.com>
2025-12-01 21:13:57 -0500

    add_and_or.asm passes all tests now

--- asm/add_and_or.asm
+++ asm/add_and_or.asm
@@ -3,14 +3,13 @@
 ; R2: result (R0 | R1 if safe, otherwise 0)
 
 START:
-    LOADI   R0, #10            ; example operand 1
-    LOADI   R1, #5             ; example operand 2
-    LOADI   R2, #0             ; default to unsafe, result = 0
-
-    AND     R3, R0, R1         ; compute R0 & R1, sets Z flag
-    BEQ     SAFE               ; if Z == 1, operation is safe
-    B       DONE               ; skip OR when unsafe
-SAFE:
-    OR      R2, R0, R1         ; safe to OR, produce "sum"
+    LOADI R0, #0xAA          ; example operand 1
+    LOADI R1, #0x55          ; example operand 2
+    LOADI R2, #0             ; default to unsafe, result = 0
+    AND   R3, R0, R1         ; compute R0 & R1, sets Z flag
+    BEQ   R3, #0, ISSAFE     ; if Z == 1, operation is safe
+    B     DONE               ; skip OR when unsafe
+ISSAFE:
+    OR    R2, R0, R1         ; safe to OR, produce "sum"
 DONE:
     HALT
